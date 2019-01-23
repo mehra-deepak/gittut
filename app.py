6f9f83513aee4a86ffae937b72e0df487a0ce8e5
Mayank Nader <mayank.nader11@gmail.com>
2019-01-23 19:11:41 +0530

    add strong tag

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return '<h1>hello</h1>'
+    return '<h1><strong>hello</strong></h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
